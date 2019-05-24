13e570de73eea8737fe6b89165967ae3d9fc334d
gkabasele <Gnicolas.KN@gmail.com>
2019-05-24 15:33:27 +0200

    Starting the reader module which extracts value from process variable

---
+++ ids/reader.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import argparse
+from queue import Queue
+
+from scapy.all import *
+from util import *
+
+class Message(object):
+
+    __slots__ = ['host', 'port', 'addr', 'size', 'value']
+
+    def __init__(self, host, kind, addr, ts):
+        self.req_timestamp = ts
+        self.res_timestamp = None
+        self.host = host
+        self.kind = kind
+        self.addr = addr
+        self.value = None
+
+class Reader(object):
+
+    def __init__(self, traces, queue):
+        self.map_req_res = {}
+        self.traces = traces
+        self.reader = PcapReader(traces)
+        self.queue = queue
+
+    def get_ip_tcp_fields(self, pkt):
+        srcip = pkt[IP].src
+        dstip = pkt[IP].dst
+        sport = pkt[TCP].sport
+        dport = pkt[TCP].dport
+        return srcip, dstip, sport, dport
+    
+    def get_modbus_req_fields(self, pkt):
+        funcode = pkt[ModbusReq].funcode
+        transId = pkt[ModbusReq].transId
+        addr = pkt[ModbusReq].startAddr
+        kind = ProcessVariable.funcode_to_kind(funcode)
+        return funcode, transId, addr, kind
+
+    def get_variable_val(self, funcode, payload):
+        val = payload.getfieldval(func_fields_dict[funcode])
+        if type(val) is list:
+            val = val[0]
+        return val
+
+    def read_packet(self, packet):
+        payload = packet.get_payload()
+        pkt = IP(payload)
+        if TCP in pkt:
+            srcip, dstip, sport, dport = self.get_ip_tcp_fields(pkt)
+            if dport == MODBUS_PORT:
+                funcode, transId, addr, kind = self.get_modbus_req_fields(pkt)
+                self.map_req_res[(transId, dstip)] = Message(dstip, kind, addr,
+                                                             packet.time)
+            elif sport == MODBUS_PORT:
+                transId = pkt[ModbusRes].transId
+                msg = self.map_req_res[(srcip, transId)] 
+                msg.value = self.get_variable_val(pkt[ModbusRes].funcode,
+                                                  pkt[ModbusRes].payload)
+                msg.res_timestamp = packet.time
+                self.queue.put(msg)
+
+    def readall(self):
+        for p in self.reader:
+            self.read_packet(p)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--trace", type=str, dest="trace")
---
+++ ids/utils.py
@@ -0,0 +1,149 @@
+import struct
+import sys
+import numpy as np
+from scapy.all import *
+
+# PORT
+MODBUS_PORT = 5020
+
+# TAG
+SRTAG_REDIRECT = 0
+SRTAG_CLONE = 1
+
+# Variable Type
+
+DIS_COIL = "co"
+DIS_INP = "di"
+HOL_REG = "hr"
+INP_REG = "ir"
+
+class ProcessVariable():
+
+    def __init__(self, host, port, kind, addr, gap=1, size=None, name=None):
+        self.host = host
+        self.port = port
+        self.kind = kind
+        self.addr = addr
+        self.name = name
+        self.size = size
+        self.gap = gap
+        self.nbr_transition = 0
+        self.last_transition = None
+        self.elapsed_time_transition = []
+        self.value = None
+
+    @classmethod
+    def funcode_to_kind(cls, funcode):
+        if funcode in [1, 5, 15]:
+            return DIS_COIL
+        elif funcode == 2:
+            return DIS_INP
+        elif funcode in [3,6,10,22,23]:
+            return HOL_REG
+        elif funcode == 4:
+            return INP_REG
+        
+
+    def __eq__(self, other):
+        return ((self.host, self.port, self.kind, self.addr) ==
+                (other.host, other.port, other.kind, other.addr))
+
+    def __hash__(self):
+        return hash((self.host, self.port, self.kind, self.addr))
+
+    def __str__(self):
+        return "%s : (ip: %s, port: %s, type: %s, addr: %s)" % (
+                                self.name, self.host, self.port, self.kind, self.addr) 
+    def __repr__(self):
+        return "%s : (ip: %s, port: %s, type: %s, addr: %s)" % (
+                                self.name, self.host,self.port, self.kind, self.addr) 
+
+    def is_bool_var(self):
+        return self.kind in [DIS_COIL, DIS_INP]
+
+class SRTag(Packet):
+    name = "SRTag"
+    fields_desc = [ IPField("dst", None),
+                    ShortField("identifier", None),
+                    ByteField("protocol", None),
+                    ByteField("reason", None)
+                  ]
+
+class ModbusReq(Packet):
+    name = "ModbusReq"
+    fields_desc = [ ShortField("transId", 0),
+                    ShortField("protoID", 0),
+                    ShortField("length", None),
+                    ByteField("unitID", 0),
+                    ByteField("funcode", None),
+                    ShortField("startAddr", 0)
+                  ]
+bind_layers(TCP, ModbusReq, dport=MODBUS_PORT)
+
+class ModbusRes(Packet):
+    name = "ModbusRes"
+    fields_desc = [ ShortField("transId", 0),
+                    ShortField("protoID", 0),
+                    ShortField("length", None),
+                    ByteField("unitID", 0),
+                    ByteField("funcode", None)
+                  ]
+
+bind_layers(TCP, ModbusRes, sport=MODBUS_PORT)
+
+class ReadCoilsRes(Packet):
+    name = "ReadCoilsRes"
+    fields_desc = [ BitFieldLenField("count", None, 8, count_of="status"),
+                    FieldListField("status", [0x00], ByteField("", 0x00), count_from=lambda x:x.count)
+                  ]
+bind_layers(ModbusRes, ReadCoilsRes, funcode=1)
+
+class ReadDiscreteRes(Packet):
+    name = "ReadDiscreteRes"
+    fields_desc = [ BitFieldLenField("count", None, 8, count_of="status"),
+                   FieldListField("status", [0x00], ByteField("", 0x00), count_from=lambda x:x.count)
+                  ]
+bind_layers(ModbusRes, ReadDiscreteRes, funcode=2)
+
+class ReadHoldRegRes(Packet):
+    name = "ReadHoldRegRes"
+    fields_desc = [ BitFieldLenField("count", None, 8, count_of="value", adjust=lambda pkt, x: x*2),
+                    FieldListField("value", [0x0000], ShortField("", 0x0000), count_from=lambda x: x.count)
+                  ]
+bind_layers(ModbusRes, ReadHoldRegRes, funcode=3)
+
+class ReadInputRes(Packet):
+    name = "ReadInputRes"
+    fields_desc = [ BitFieldLenField("count", None, 8, count_of="registers", adjust=lambda pkt, x: x*2),
+                    FieldListField("registers", [0x0000], ShortField("", 0x0000), count_from=lambda x:x.count)
+                  ]
+bind_layers(ModbusRes, ReadInputRes, funcode=4)
+
+class WriteSingleCoilRes(Packet):
+    name = "WriteSingleCoilRes"
+    fields_desc = [ ShortField("addr",None),
+                    ShortField("value",None)
+                  ]
+bind_layers(ModbusRes, WriteSingleCoilRes, funcode=5)
+
+class WriteSingleRegRes(Packet):
+    name = "WriteSingleRegRes"
+    fields_desc = [ ShortField("addr", None),
+                    ShortField("value", None)
+                  ]
+bind_layers(ModbusRes, WriteSingleRegRes, funcode=6)
+
+
+# Translation between funcode and field name
+func_fields_dict = {
+                     1 : "status",
+                     2 : "status",
+                     3 : "value", 
+                     4 : "registers",
+                     5 : "value",
+                     6 : "value",
+                   }
+
+def is_number(s):
+    """ Returns Truse if string s is a number """
+    return s.replace('.','',1).isdigit()

d5fceef9b2986a433cfdf129be11d9b3f3d3db36
gkabasele <Gnicolas.KN@gmail.com>
2019-12-19 14:31:45 +0100

    optimization of mining rules

--- ids/cfpgrowth/src/algorithms/AssociationRuleMining.java
+++ ids/cfpgrowth/src/algorithms/AssociationRuleMining.java
@@ -1,7 +1,11 @@
 package  algorithms;
 import com.sun.corba.se.spi.ior.IORTemplate;
 
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.Map.Entry;
@@ -9,7 +13,10 @@ import java.lang.Integer;
 
 import java.io.*;
 
-class ItemSet {
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+class ItemSet implements Serializable {
 	private Set<Short> items;
 	private short support;
 	private boolean isClose;
@@ -91,19 +98,29 @@ public class AssociationRuleMining {
 
 	private List<ItemSet> closeItemsets;
 	private List<AssociationRule> rules;
+	// Items ID -> Items
+	private Map<Short, Short> itemsMap;
 
-	public static short NOTFOUND = -1;
+	public static final short NOTFOUND = -1;
+	public static final int CHACHESIZE = 1000000;
+	// Constant the differentiate case where a the support of the cause
+	// is the same as the support of the effect
+	public static final byte SAMESUP = 1;
+	public static final byte GREATERSUP = 2;
+	public static final byte LESSERSUP = 3;
+	public static final byte CONTINUE = 4;
 
 	public AssociationRuleMining(){
 		rules = new ArrayList<>();
 		closeItemsets = new ArrayList<>();
+		itemsMap = new HashMap<>();
 	}
 
 	public List<ItemSet> getCloseItemsets() {
 		return closeItemsets;
 	}
 
-	public void fillCloseItemSet(String input) throws IOException{
+	public void fillCloseItemSetFromFreq(String input) throws IOException{
 		// Read File line by line
 
 		String line = null;
@@ -130,7 +147,80 @@ public class AssociationRuleMining {
 		long endTime = System.nanoTime();
 		double duration = (endTime - startTime)/1000000000;
 		System.out.println("Duration (s): " + duration);
-		closeItemsets = candidates;
+		candidates.sort(Comparator.comparingInt(ItemSet::getSupport));
+		closeItemsets =  candidates;
+	}
+
+	public void importItemsSupport(String support) throws IOException {
+		String line;
+		String[] values;
+		short itemsID;
+		short itemsSupport;
+		try(FileInputStream inputStream = new FileInputStream(support); Scanner sc = new Scanner(inputStream, "UTF-8")){
+			while(sc.hasNextLine()){
+				line = sc.nextLine();
+				values = line.split(" ");
+				itemsID = Short.parseShort(values[0]);
+				itemsSupport = Short.parseShort(values[1]);
+				itemsMap.put(itemsID, itemsSupport);
+			}
+		}
+	}
+
+	public void importCloseItemSets (String closeInput) throws IOException {
+		String line;
+		ItemSet itemSet;
+		try(FileInputStream inputStream = new FileInputStream(closeInput); Scanner sc = new Scanner(inputStream, "UTF-8")){
+			while (sc.hasNextLine()){
+				line = sc.nextLine();
+				itemSet  = stringToItemSet(line);
+				closeItemsets.add(itemSet);
+			}
+		}
+		closeItemsets.sort(Comparator.comparingInt(ItemSet::getSupport));
+	}
+
+	public void writeItemsetsToFileBin(String input, String output){
+		String line;
+		ItemSet itemSet;
+		try{
+			FileInputStream inputStream = new FileInputStream(input);
+			Scanner sc = new Scanner(inputStream, "UTF-8");
+
+			ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(Paths.get(output), CREATE, APPEND));
+			while(sc.hasNextLine()){
+				line = sc.nextLine();
+				itemSet = stringToItemSet(line);
+				out.writeObject(itemSet);
+			}
+			out.close();
+
+		} catch (IOException ie){
+			System.err.println("Could not read file: " + output);
+		}
+
+	}
+
+	public void readItemsetsFromBin(String input){
+		try{
+			FileInputStream inputStream = new FileInputStream(input);
+			ObjectInputStream in = new ObjectInputStream(inputStream);
+
+			while(true){
+				ItemSet itemSet = (ItemSet) in.readObject();
+				System.out.println(itemSet);
+			}
+		} catch (EOFException eof){
+			System.out.println("Reach end of file");
+		} catch (ClassNotFoundException cle){
+			System.err.println("Could not serialize process");
+		} catch (IOException e){
+			System.err.println("Find could not be found");
+		}
+	}
+
+	public void miningRulefromClose(String freqInput, boolean binaryFile){
+		createRulesFromCloseItemset(freqInput, binaryFile);
 	}
 
 	public void exportCloseItemsets(String filename) throws IOException{
@@ -204,7 +294,7 @@ public class AssociationRuleMining {
 		short support = Short.parseShort(values[1].replaceAll("\\s", ""));
 		String[] it = values[0].split("#");
 		String[] itemsID = it[0].split(" ");
-		Set<Short> items = new HashSet<>(itemsID.length);
+		Set<Short> items = new LinkedHashSet<>(itemsID.length);
 		for (String s: itemsID){
 			items.add(Short.parseShort(s));
 		}
@@ -262,6 +352,37 @@ public class AssociationRuleMining {
 		}
 	}
 
+	private void createRulesFromCloseItemset(String filename, boolean binaryFile){
+		long startTime = System.nanoTime();
+		LinkedHashMap<Set<Short>, Short> cache = new LinkedHashMap<Set<Short>, Short>(){
+			@Override
+			protected boolean removeEldestEntry(Entry entry) {
+				return size() > CHACHESIZE;
+			}
+		};
+		int index = 1;
+		for (ItemSet itemSet : closeItemsets){
+			System.out.println("Starting close itemset: (" + index + "/" + closeItemsets.size() + ")");
+			if (isRelevantCloseItemSet(itemSet)){
+				rulesFromSet(itemSet, filename, cache, binaryFile);
+			} else{
+				System.out.println("Irrelevant close itemset");
+			}
+			index += 1;
+		}
+		double duration = (System.nanoTime() - startTime)/100000000;
+		System.out.println("Duration (s): " + duration);
+	}
+
+	// Assume that the itemset is sorted by the support of its items
+	private boolean isRelevantCloseItemSet(ItemSet itemSet) {
+		List<Short> itemsIDList = new ArrayList<>(itemSet.getItems());
+		if (itemSet.length() < 2)
+		    return false;
+		return (itemsMap.get(itemsIDList.get(0)) == itemSet.getSupport() && itemsMap.get(itemsIDList.get(1)) == itemSet.getSupport());
+	}
+
+
 	private boolean detectCloseItemset(byte size, byte maxKey, ItemSet itemset, Map<Byte, List<ItemSet>> map){
 		boolean hasSuperset;
 		boolean isClose;
@@ -286,6 +407,96 @@ public class AssociationRuleMining {
 		return true;
 	}
 
+	private byte checkSupport(short causeSupport, short effectSupport){
+		if (causeSupport == effectSupport){
+			return SAMESUP;
+		} else if (causeSupport > effectSupport){
+			return GREATERSUP;
+		} else {
+			return LESSERSUP;
+		}
+	}
+
+	private byte sameSupport(Set<Short> cause, Set<Short> effect, String filename,
+								LinkedHashMap<Set<Short>, Short> cache, boolean binaryFile) throws IOException{
+		short causeSupport = -1;
+		short effectSupport = -1;
+
+		if (cache.containsKey(cause))
+			causeSupport = cache.get(cause);
+
+		if (cache.containsKey(effect))
+			effectSupport = cache.get(effect);
+
+		if (causeSupport > -1 && effectSupport > -1)
+			return checkSupport(causeSupport, effectSupport);
+
+		if(binaryFile)
+			return findCauseEffectBin(cause, effect, filename, cache);
+		else
+			return findCauseEffectText(cause, effect, filename, cache);
+	}
+
+	private byte findCauseEffectBin(Set<Short> cause, Set<Short>effect, String filename,
+									LinkedHashMap<Set<Short>, Short> cache){
+		short causeSupport = -1;
+		short effectSupport = -1;
+		try{
+			FileInputStream inputStream = new FileInputStream(filename);
+			ObjectInputStream in = new ObjectInputStream(inputStream);
+
+			while(true){
+				ItemSet itemSet = (ItemSet) in.readObject();
+				cache.putIfAbsent(itemSet.getItems(), itemSet.getSupport());
+				if(itemSet.getItems().equals(cause)){
+					causeSupport = itemSet.getSupport();
+				} else if (itemSet.getItems().equals(effect)){
+					effectSupport = itemSet.getSupport();
+				}
+
+				if (causeSupport > -1 && effectSupport > -1){
+					inputStream.close();
+					return checkSupport(causeSupport, effectSupport);
+				}
+			}
+		} catch (EOFException eof){
+		    return CONTINUE;
+		} catch (ClassNotFoundException cle){
+
+		} catch (IOException e){
+
+		}
+		return CONTINUE;
+	}
+
+	private byte findCauseEffectText(Set<Short> cause, Set<Short>effect, String filename,
+								 LinkedHashMap<Set<Short>, Short> cache) throws IOException{
+		String line;
+		ItemSet itemset;
+		short causeSupport = -1;
+		short effectSupport = -1;
+		try (FileInputStream inputStream = new FileInputStream(filename); Scanner sc = new Scanner(inputStream, "UTF-8")){
+			while (sc.hasNextLine()){
+				line = sc.nextLine();
+				itemset = stringToItemSet(line);
+				cache.putIfAbsent(itemset.getItems(), itemset.getSupport());
+				if (itemset.getItems().equals(cause)){
+					causeSupport = itemset.getSupport();
+				} else if (itemset.getItems().equals(effect)){
+					effectSupport = itemset.getSupport();
+				}
+
+				if (causeSupport > -1 && effectSupport > -1){
+					inputStream.close();
+					return checkSupport(causeSupport, effectSupport);
+				}
+			}
+			inputStream.close();
+		}
+
+		return CONTINUE;
+	}
+
 	private short findSupport(Set<Short> set, Map<Byte, List<ItemSet>> map){
 		for (Entry<Byte, List<ItemSet>> entry: map.entrySet()){
 		    if(entry.getKey() == set.size()){
@@ -297,6 +508,13 @@ public class AssociationRuleMining {
 			}
 		}
 		return NOTFOUND;
+	}
+
+	private byte divideFromLength(Short[] itemSetList, int length, String filename,
+									 LinkedHashMap<Set<Short>, Short> cache, boolean binaryFile){
+		Short[] cause = new Short[length];
+		short root = itemSetList[0];
+		return itemSetDivision(itemSetList, root, length, 0, cause, 0, filename, cache, binaryFile);
 	}
 
 	private boolean divideFromLength(Short[] itemSetList, int length, Map<Byte, List<ItemSet>> map){
@@ -313,6 +531,39 @@ public class AssociationRuleMining {
 			}
 		}
 		return effect;
+	}
+
+	private byte  itemSetDivision(Short[] itemSetList, short root, int length, int index, Short[] cause, int i,
+									 String filename, LinkedHashMap<Set<Short>, Short> cache, boolean binaryFile){
+		byte res;
+		if (index == length){
+			List<Short> causelist = Arrays.asList(cause);
+			List<Short> items = Arrays.asList(itemSetList);
+			List<Short> effect = getEffectFromCause(items, causelist);
+			try {
+				res = createRule(new HashSet<>(causelist), new HashSet<>(effect), filename, cache, binaryFile);
+				return res;
+			} catch (IOException e) {
+				System.out.println("File error for " + causelist + " -> " + effect);
+			}
+		}
+
+		if (i >= itemSetList.length){
+			return CONTINUE;
+		}
+
+		cause[index] = itemSetList[i];
+		if (cause[0] != root){
+			return CONTINUE;
+		}
+		res = itemSetDivision(itemSetList, root, length, index + 1, cause, i + 1, filename, cache, binaryFile);
+		if (res != SAMESUP){
+			byte tmp = itemSetDivision(itemSetList, root, length, index, cause, i + 1, filename, cache, binaryFile);
+			if (tmp != res && (tmp == SAMESUP || tmp == LESSERSUP)){
+				res = tmp;
+			}
+		}
+		return res;
 	}
 
 	private boolean itemSetDivision(Short[] itemSetList, short root, int length, int index, Short[] cause, int i,
@@ -341,6 +592,17 @@ public class AssociationRuleMining {
 		return res;
 	}
 
+	private byte createRule(Set<Short> cause, Set<Short> effect, String filename,
+							   LinkedHashMap<Set<Short>, Short> cache, boolean binaryFile) throws IOException {
+		byte same = sameSupport(cause, effect, filename, cache, binaryFile);
+		if (same == SAMESUP){
+			AssociationRule rule = new AssociationRule(new ArrayList<>(cause), new ArrayList<>(effect));
+			System.out.println("Add rules: " + rule);
+			rules.add(rule);
+		}
+		return same;
+	}
+
 	private boolean createRule(Set<Short> cause, Set<Short> effect, Map<Byte, List<ItemSet>> map){
 		short cisSupport = findSupport(cause, map);
 		short eisSupport = findSupport(effect, map);
@@ -353,10 +615,26 @@ public class AssociationRuleMining {
 		return false;
 	}
 
+	private void rulesFromSet(ItemSet itemset, String filename,
+							  LinkedHashMap<Set<Short>, Short> cache, boolean binaryFile){
+		byte res;
+		Short[] array = new Short[itemset.length()];
+		array = (new ArrayList<>(itemset.getItems())).toArray(array);
+
+
+		for(int i=1; i < itemset.length(); i++){
+		    System.out.println("Cause of length  (" + i + "/" + itemset.length()+")");
+			res = divideFromLength(array, i, filename, cache, binaryFile);
+			if (res == SAMESUP || res == LESSERSUP  ){
+				return;
+			}
+		}
+	}
+
 	private void rulesFromSet(ItemSet itemset, Map<Byte, List<ItemSet>> map){
 		boolean res;
 		Short[] array = new Short[itemset.length()];
-		array = (new ArrayList<Short>(itemset.getItems())).toArray(array);
+		array = (new ArrayList<>(itemset.getItems())).toArray(array);
 
 		for(int i=1; i < itemset.length(); i++){
 		   	res = divideFromLength(array, i, map);
@@ -365,6 +643,4 @@ public class AssociationRuleMining {
 			}
 		}
 	}
-
-
 }
--- ids/cfpgrowth/src/algorithms/MainTestAllAssociationRules_CFPGrowth_saveToFile.java
+++ ids/cfpgrowth/src/algorithms/MainTestAllAssociationRules_CFPGrowth_saveToFile.java
@@ -14,19 +14,52 @@ import java.util.Map;
 public class MainTestAllAssociationRules_CFPGrowth_saveToFile {
 
 
+	public static void getFrequentItemsets(String transactions, String MIS, String output) throws IOException {
+		AlgoCFPGrowth cfpGrowth = new AlgoCFPGrowth();
+		cfpGrowth.runAlgorithm(transactions, output, MIS);
+		cfpGrowth.printStats();
+	}
+
+	public static void approachList(String frequentItemsets, String closeOutput) throws IOException {
+		AssociationRuleMining miner = new AssociationRuleMining();
+		System.out.println("Looking for close itemset");
+		miner.fillCloseItemSetFromFreq(frequentItemsets);
+		System.out.println("Exporting close itemset to : " + closeOutput);
+		miner.exportCloseItemsets(closeOutput);
+	}
+
+	public static void mineRuleFromCloseItemSets(String frequentItemsets, String support, String closeOutput,
+												 String invariants, boolean binaryFile) throws IOException {
+		AssociationRuleMining miner = new AssociationRuleMining();
+		miner.importItemsSupport(support);
+		System.out.println("Importing close itemsets from: " + closeOutput);
+		miner.importCloseItemSets(closeOutput);
+		System.out.println("Mining rule from using support from: " + frequentItemsets);
+		miner.miningRulefromClose(frequentItemsets, binaryFile);
+		System.out.println("Exporting invariants to : " + invariants);
+		miner.exportRule(invariants);
+	}
+
+	public static void approachMap(String frequentItemsets, String closeOutput, String invariants) throws IOException {
+		AssociationRuleMining miner = new AssociationRuleMining();
+		Map<Byte, List<ItemSet>> map = miner.miningRules(frequentItemsets);
+		miner.exportCloseItemsets(closeOutput, map);
+		System.out.println("Exporting invariants");
+		miner.exportRule(invariants);
+	}
 
 	public static void main(String [] arg) throws IOException{
 		File directory = new File("./");
 		System.out.println(directory.getAbsolutePath());
-		//String transactions = fileToPath("/textfiles/test_close_transactions.txt");
+		//String transactions = fileToPath("/textfiles/database_complete.txt");
 		String transactions = fileToPath("/textfiles/database.txt");
-		//String frequentItemsets = ".//test_frequent.txt";
-		String frequentItemsets = ".//output.txt";
-		//String filtered_output = ".//test_close_frequent_output.txt";
-		String filtered_output = ".//output_filtered";
-		//String MISfile = fileToPath("/textfiles/test_mis_close.txt");
+		//String frequentItemsetsComplete = ".//output_complete.txt";
+		String frequentItemsets= ".//output.txt";
+		//String filtered_output = ".//output_filtered";
+		//String MISfileComplete = fileToPath("/textfiles/mis_complete.txt");
 		String MISfile = fileToPath("/textfiles/mis_v2.txt");
-		String closeOutput = ".//close_output_map.txt";
+		//String closeOutputComplete = ".//close_output_complete.txt";
+		String closeOutput= ".//close_output_sorted.txt";
 		String invariants = ".//invariants.txt";
 
 		// Check how to input is organised to determine if it need some prepocessing
@@ -37,29 +70,23 @@ public class MainTestAllAssociationRules_CFPGrowth_saveToFile {
 		*/
 
 		// STEP 1: Applying the CFP-GROWTH algorithm to find frequent itemsets
-		//AlgoCFPGrowth cfpgrowth = new AlgoCFPGrowth();
-		//cfpgrowth.runAlgorithm(transactions, frequentItemsets, MISfile);
-
-		//double minsup =  0.1 * cfpgrowth.getDatabaseSize();
-		//cfpgrowth.printStats();
+		//getFrequentItemsets(transactions, MISfile, frequentItemsets);
 
 		AssociationRuleMining miner = new AssociationRuleMining();
-		//System.out.println("Filtering frequent request with minsup: " + minsup);
-		//miner.filterItemSets(frequentItemsets, filtered_output, minsup);
+		String binFile = ".//output.bin";
+		File f = new File(binFile);
+		if(f.delete()){
+			System.out.println("deleting file " + binFile);
+		}
 
-		System.out.println("Mining the invariants");
 
 		// Approach 1 : Keep a list of the close itemset so far and look with new itemset if list must be updated
-		//miner.fillCloseItemSet(frequentItemsets);
-		//miner.exportCloseItemsets(closeOutput);
-
+		miner.writeItemsetsToFileBin(frequentItemsets, binFile);
+		mineRuleFromCloseItemSets(binFile, MISfile, closeOutput, invariants, true);
 
 		// Approach 2 : Store every itemset then look which one are closed
-		Map<Byte, List<ItemSet>> map = miner.miningRules(frequentItemsets);
-		miner.exportCloseItemsets(closeOutput, map);
+		//approachMap(frequentItemsets, closeOutput, invariants);
 
-		System.out.println("Exporting invariants");
-		miner.exportRule(invariants);
 		System.out.println("Done");
 	}
 	

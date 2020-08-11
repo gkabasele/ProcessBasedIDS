FIX autoregressive model, vector of past value was in the wrong order

ids/autoreg.py
@@ -1,5 +1,7 @@
 import pdb
 import matplotlib.pyplot as plt
+import scipy.stats as st
+from statsmodels.graphics.tsaplots import plot_acf
 import numpy as np
 from ar import arsel
 
@@ -16,6 +18,7 @@ class ARpredictor(object):
         self.def_free = None
 
     def train(self, data):
+        # arsel(data, submean=True, absrho=True, criterion=CIC, minorder=0, maxorder=512)
         a = arsel(data, False, True, "AIC")
         self.coef = [-x for x in a.AR[0]]
 
@@ -40,13 +43,32 @@ class ARpredictor(object):
 
         while j < len(dataset):
             value = dataset[i:j]
-            pred = self.predict(value)
+            pred = self.predict(value[::-1])
             residuals.append(dataset[j] - pred)
             predictions.append(pred)
             i += 1
             j += 1
 
         self.deg_free = len(residuals) - 1
+
+        plt.plot(range(len(residuals)), residuals)
+        plt.show()
+
+        # the residuals should not appear as autocorrelated, they should be
+        # white noise
+
+        plot_acf(residuals, lags=90, fft=True)
+        plt.title("Residual AC")
+        plt.show()
+
+        plt.hist(residuals, density=True, bins=30)
+        plt.title("Histogram residuals")
+        mn, mx = plt.xlim()
+        plt.xlim(mn, mx)
+        kde_xs = np.linspace(mn, mx, 301)
+        kde = st.gaussian_kde(residuals)
+        plt.plot(kde_xs, kde.pdf(kde_xs))
+        plt.show()
 
         self.residual_mean = np.mean(residuals)
         self.residual_var = np.var(residuals)
@@ -59,9 +81,14 @@ def compute_coef_and_predict(filename, split_index):
         data = [float(value) for value in f]
 
         train, test = data[:split_index], data[split_index+1:]
+        #train, test = data, data
+        # train should be autocorrelated
+        plot_acf(train, lags=90, fft=True)
+        plt.show()
 
         model = ARpredictor()
         model.train(train)
+        pdb.set_trace()
         predictions = model.make_predictions_from_test(test)
 
         mu_o = np.mean(test)
@@ -71,7 +98,6 @@ def compute_coef_and_predict(filename, split_index):
 
         upper_limit = mu_o + 3*std_o
         lower_limit = mu_o - 3*std_o
-
 
         print("mu_o:{}, mu_p:{}".format(mu_o, mu_p))
         print("var_o:{}, var_p:{}".format(np.var(test), np.var(predictions)))
@@ -84,5 +110,7 @@ def compute_coef_and_predict(filename, split_index):
         plt.axhline(y=lower_limit, color='orange', linestyle='-')
         plt.show()
 
-#compute_coef_and_predict("../autoregressive_opensource/test1.dat", 100)
-compute_coef_and_predict("../SWaT_scripts/lit301_normal.dat", 209700) # 419 400/209 700
+if __name__ == "__main__":
+    #compute_coef_and_predict("../../autoregressive_opensource/test1.dat", 100)
+    compute_coef_and_predict("../SWaT_scripts/lit301_normal.dat", 209700) # 419 400/209 700
+    #compute_coef_and_predict("../SWaT_scripts/tank1_normal_1h.dat", 1000) # 300/2

ids/idsAR.py
@@ -3,23 +3,24 @@
 from collections import OrderedDict
 from collections import deque
 from datetime import datetime
+from argparse import ArgumentParser
+import pdb
 import numpy as np
+from scipy.stats import f
 from autoreg import ARpredictor
 from reqChecker import Checker
 from welford import Welford
-from scipy.stats import f
+import utils
 
 class IDSAR(Checker):
 
-    def __init__(self, descFile, store, detection_store, control_coef=3, alpha=0.05):
+    def __init__(self, descFile, store, control_coef=3, alpha=0.05):
         #alpha : signifiance level
         #control_coef: Stewart control limit
 
         Checker.__init__(self, descFile, store)
 
         self.map_pv_predictor = dict()
-
-        self.detection_store = None
 
         self.malicious_activities = OrderedDict()
 
@@ -36,7 +37,7 @@ class IDSAR(Checker):
         map_pv_col = {x: list() for x in self.map_pv_predictor.keys()}
         for _, state in enumerate(self.store):
             for name, val in state.items():
-                if name in self.map_pv_col:
+                if name in map_pv_col:
                     map_pv_col[name].append(val)
 
         for k, data in map_pv_col.items():
@@ -50,15 +51,16 @@ class IDSAR(Checker):
             model.upper_limit = mu + self.control_coef * std
             model.lower_limit = mu - self.control_coef * std
 
-    def run_detection_mode(self):
-        if self.detection_store is None:
-            raise(ValueError("No data to run the detection on"))
+    def run_detection_mode(self, detection_store, debug_file=None):
 
         map_pv_hist = {k: deque(maxlen=v.order()) for k, v in self.map_pv_predictor.items()}
         map_pv_pred_err = {k: Welford() for k in self.map_pv_predictor.keys()}
 
-        for i, state in enumerate(self.detection_store):
+        for i, state in enumerate(detection_store):
             ts = state["timestamp"]
+            if debug_file is not None:
+                debug_file.write("State nbr:{}\n".format(i))
+
             for name, val in state.items():
                 if name not in self.map_pv_predictor:
                     continue
@@ -73,7 +75,7 @@ class IDSAR(Checker):
                     if self.anomaly_detected(name, val, map_pv_pred_err):
                         self.add_malicious_activities(ts, name)
 
-                map_pv_hist[name].add(val)
+                map_pv_hist[name].append(val)
 
     def add_malicious_activities(self, ts, name):
 
@@ -82,22 +84,57 @@ class IDSAR(Checker):
         if time_key not in self.malicious_activities:
             self.malicious_activities[time_key] = set()
 
-        malicious_activities[time_key].add(name)
+        self.malicious_activities[time_key].add(name)
 
     def anomaly_detected(self, name, val, pred_err):
 
         model = self.map_pv_predictor[name]
         if val > model.upper_limit or val < model.lower_limit:
+            pdb.set_trace()
             return True
 
-        F = model.residuals_var/(pred_err.std**2)
-        df1 = model.deg_free
-        df2 = pred_err.k - 1
+        try:
+            F = model.residual_var/(pred_err[name].std**2)
+            df1 = model.deg_free
+            df2 = pred_err[name].k - 1
 
-        # H0: The variances of the residual during the training phase and the detection phase are the same
-        # Ha: The variances are different
+            # H0: The variances of the residual during the training phase and the detection phase are the same
+            # Ha: The variances are different
 
-        crit1 = f.ppf(1-self.alpha/2, df1, df2)
-        crit2 = f.ppf(self.alpha/2, df1, df2)
+            crit1 = f.ppf(1-self.alpha/2, df1, df2)
+            crit2 = f.ppf(self.alpha/2, df1, df2)
 
-        return (F < crit1 or F > crit2)
+            if F < crit1 or F > crit2:
+                pdb.set_trace()
+                return True
+            return False
+        except ZeroDivisionError:
+            return False
+
+def main(normal_file, attack_file, conf_file, mal_file):
+    data_norm = utils.read_state_file(normal_file)
+    data_mal = utils.read_state_file(attack_file)
+    pdb.set_trace()
+    ids = IDSAR(conf_file, data_norm)
+    ids.create_predictors()
+    ids.train_predictors()
+    pdb.set_trace()
+    with open(mal_file, "w") as f:
+        ids.run_detection_mode(data_mal)
+
+        for k, v in ids.malicious_activities.items():
+            f.write("{} : {}\n".format(k, v))
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--normal", dest="normal_file", action="store",
+                        help="file containing normal process")
+    parser.add_argument("--attack", dest="attack_file", action="store",
+                        help="file containing attack process")
+    parser.add_argument("--malicious", dest="mal_file", action="store",
+                        help="file where to export detection")
+    parser.add_argument("--conf", dest="conf", action="store",
+                        help="process variable conf file")
+    args = parser.parse_args()
+
+    main(args.normal_file, args.attack_file, args.conf, args.mal_file)
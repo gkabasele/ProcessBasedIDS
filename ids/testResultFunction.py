e67bd45e934aa61f019ef11accfd2659aaee3230
gkabasele <Gnicolas.KN@gmail.com>
2020-10-29 17:57:16 +0100

    scenario for test result function

--- ids/testResultFunction.py
+++ ids/testResultFunction.py
@@ -64,13 +64,39 @@ def test_get_ids_result():
     malicious[datetime(2020, 10, 22, 10, 15, 10)] = True
     malicious[datetime(2020, 10, 22, 10, 20, 10)] = False
 
-    detected, wrong, missed =  get_ids_result(malicious.keys(), p)
+    detected, wrong, missed = get_ids_result(malicious.keys(), p)
 
-    
     assert detected == 1
     assert missed == 2
     assert wrong == 1
 
     malicious.clear()
+
+    #scenario 5
+    malicious[datetime(2020, 10, 22, 9, 0, 0)] = False
+    malicious[datetime(2020, 10, 22, 10, 0, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 1, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 2, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 3, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 4, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 5, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 8, 0)] = False
+    malicious[datetime(2020, 10, 22, 10, 9, 0)] = False
+    malicious[datetime(2020, 10, 22, 10, 9, 30)] = False
+    malicious[datetime(2020, 10, 22, 10, 10, 0)] = True
+    malicious[datetime(2020, 10, 22, 10, 10, 15)] = True
+    malicious[datetime(2020, 10, 22, 10, 10, 29)] = True
+    malicious[datetime(2020, 10, 22, 10, 10, 40)] = False
+    malicious[datetime(2020, 10, 22, 10, 13, 29)] = False
+
+    malicious[datetime(2020, 10, 22, 10, 15, 10)] = True
+    malicious[datetime(2020, 10, 22, 10, 15, 59)] = True
+    malicious[datetime(2020, 10, 22, 10, 16, 10)] = False
+
+    detected, wrong, missed = get_ids_result(malicious.keys(), p)
+
+    assert detected == 3
+    assert missed == 0
+    assert wrong == 7
 
 test_get_ids_result()
